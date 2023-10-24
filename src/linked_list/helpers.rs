c79dce20eceb55162693f13b63a1266e733c78a9
dakamakat <gazon-ozon@yandex.ru>
2023-10-24 16:17:38 +0300

    feat(linked_list): update models / add print helpers

--- src/linked_list/helpers.rs
+++ src/linked_list/helpers.rs
@@ -6,8 +6,16 @@ pub fn array_to_linked_list(arr: &[i32]) -> Option<Box<ListNode>> {
     let mut head = None;
 
     for n in arr {
-        let new_node = ListNode::new(*n , head);
+        let new_node = ListNode::new(*n, head);
         head = Some(Box::new(new_node))
     }
     head
+}
+
+pub fn print_linked_list(head: Option<Box<ListNode>>) {
+    let mut curr = head;
+    while let Some(node) = curr {
+        curr = node.next;
+        println!("{0}" , node.val);
+    }
 }
