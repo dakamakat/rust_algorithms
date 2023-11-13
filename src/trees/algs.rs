de4e1fd0f4403a97abe75f06f331467a647d985a
dakamakat <gazon-ozon@yandex.ru>
2023-11-13 12:46:51 +0100

    feat(Tree / cousing): implement binary search / check cousins alg for binary tree

--- src/trees/algs.rs
+++ src/trees/algs.rs
@@ -1,25 +1,76 @@
 #![allow(dead_code)]
 
 use super::models::TreeNode;
-use std::{cell::RefCell, rc::Rc, cmp::Ordering};
+use std::{cell::RefCell, cmp::Ordering, rc::Rc};
 
 type OptNode = Option<Rc<RefCell<TreeNode>>>;
 
+pub fn find_bt_node_depth(root: Option<Rc<RefCell<TreeNode>>>, val: i32) -> i32 {
+    let mut depth = -1;
+    if let Some(node) = root {
+        let rc_node = node.clone();
+        let value = rc_node.borrow();
 
+        if value.val == val {
+            return depth + 1;
+        }
+
+        depth = find_bt_node_depth(value.left.clone(), val);
+
+        if depth >= 0 {
+            return depth + 1;
+        }
+
+        depth = find_bt_node_depth(value.right.clone(), val);
+        if depth >= 0 {
+            return depth + 1;
+        }
+    }
+    depth
+}
 pub fn search_bst(root: Option<Rc<RefCell<TreeNode>>>, val: i32) -> Option<Rc<RefCell<TreeNode>>> {
     match root {
         Some(node) => {
-            let value = node.clone();
+            let rc_node = node.clone();
+            let value = rc_node.borrow();
 
-            if value.borrow().val == val {
+            if value.val == val {
                 return Some(node);
-            } else if value.borrow().val < val {
-                search_bst(value.borrow().right.clone(), val)
-            } else if value.borrow().val > val {
-                search_bst(value.borrow().left.clone(), val)
+            } else if value.val < val {
+                search_bst(value.right.clone(), val)
+            } else if value.val > val {
+                search_bst(value.left.clone(), val)
             } else {
                 None
             }
+        }
+        None => None,
+    }
+}
+
+pub fn search_bt(root: Option<Rc<RefCell<TreeNode>>>, val: i32) -> Option<Rc<RefCell<TreeNode>>> {
+    match root {
+        Some(node) => {
+            let rc_node = node.clone();
+            let value = rc_node.borrow();
+
+            if value.val == val {
+                return Some(node);
+            }
+
+            let left = search_bt(value.left.clone(), val);
+
+            if left.is_some() {
+                return left;
+            }
+
+            let right = search_bt(value.right.clone(), val);
+
+            if right.is_some() {
+                return right;
+            }
+
+            None
         }
         None => None,
     }
@@ -37,4 +88,39 @@ pub fn search_bst_iter(root: OptNode, val: i32) -> OptNode {
         }
     }
     None
+}
+
+pub fn is_cousins(root: Option<Rc<RefCell<TreeNode>>>, x: i32, y: i32) -> bool {
+    fn find_bt_node_depth(
+        root: Option<Rc<RefCell<TreeNode>>>,
+        prevNodeVal: i32,
+        val: i32,
+    ) -> (i32, i32) {
+        let mut depth = (prevNodeVal, -1);
+        if let Some(node) = root {
+            let rc_node = node.clone();
+            let value = rc_node.borrow();
+
+            if value.val == val {
+                return (prevNodeVal, depth.1 + 1);
+            }
+
+            depth = find_bt_node_depth(value.left.clone(), value.val, val);
+
+            if depth.1 >= 0 {
+                return (depth.0, depth.1 + 1);
+            }
+
+            depth = find_bt_node_depth(value.right.clone(), value.val, val);
+            if depth.1 >= 0 {
+                return (depth.0, depth.1 + 1);
+            }
+        }
+        (prevNodeVal, depth.1)
+    }
+
+    let x_depth = find_bt_node_depth(root.clone(), root.clone().unwrap().borrow().val, x);
+    let y_depth = find_bt_node_depth(root.clone(), root.clone().unwrap().borrow().val, y);
+
+    matches!((x_depth.0 != y_depth.0) && (x_depth.1 == y_depth.1), true)
 }
